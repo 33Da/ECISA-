82c9b783d97f5065b015af2c61d4b906ccecc72d
33Da <764720843@qq.com>
2021-06-27 15:59:54 +0800

    first commit

---
+++ 数据提取.py
@@ -0,0 +1,45 @@
+from xml.dom.minidom import parse
+import xml.dom.minidom
+import pandas as pd
+
+
+DOMTree = xml.dom.minidom.parse("data/SMP2019_ECISA_Dev.xml")
+collection = DOMTree.documentElement
+
+docs = collection.getElementsByTagName("Doc")
+
+result = {
+    "text": [],
+    "pre_text": [],
+    "last_pre": [],
+    "label": []
+}
+
+for doc in docs:
+
+    sentences = doc.getElementsByTagName("Sentence")
+    pre = ""
+    last = ""
+    flag = True
+    text = None
+    label = None
+    for sentence in sentences:
+        if sentence.hasAttribute("label"):
+            text = sentence.childNodes[0].data
+            label = sentence.getAttribute("label")
+            flag = False
+        if flag:
+            pre += sentence.childNodes[0].data + "###"
+        if not flag:
+            last += sentence.childNodes[0].data + "###"
+
+    result["text"].append(text)
+    result["pre_text"].append(pre)
+    result["last_pre"].append(last)
+    result["label"].append(label)
+
+
+df = pd.DataFrame(result)
+df.to_csv("data/dev.csv")
+
+
